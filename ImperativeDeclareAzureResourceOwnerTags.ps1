9b9dd470ccc0973e8a459630f3f373c3f52c3000
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2017-12-21 15:29:02 -0500

    Update ImperativeDeclareAzureResourceOwnerTags.ps1

--- ImperativeDeclareAzureResourceOwnerTags.ps1
+++ ImperativeDeclareAzureResourceOwnerTags.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
     .DESCRIPTION
         An runbook which finds the deployment owner of a Azure resource and applies an owner Tag to the Azure resource gets all the ARM resources using the Run As Account (Service Principal)
 
@@ -34,7 +34,7 @@ catch {
 
 # Obtain resource objects
 ##Needs## Filter out existing Azure Resource objects that already have Owner Tag assigned
-$azurerescs = Get-AzureRmResource # | Where-Object ResourceGroupName -contains rgMDTest2 ## < Filter to apply on a RG
+$azurerescs = Get-AzureRmResource | Where-Object ResourceGroupName -contains rgBBOPS ## < Filter to apply on a RG
 
 # Execute through each resource object and apply a owner tag based on who deployed the resource
 ##Needs## Rewrite to use parallelization using either: PS WorkFlow OR Start-Job > https://github.com/Azure/azure-powershell/releases/tag/v4.4.0-September2017
@@ -44,14 +44,22 @@ foreach ($azureresc in $azurerescs){
 $azurerescop = $azureresc.ResourceType + "/write"
 
 #find operations where resource was created in past 90 days # Activity Log only goes back 90 days unless stored in a db of some sort for historical \ archival purposes
-$azurerescvalues = Get-AzureRmLog -StartTime (Get-Date).AddDays(-90) -ResourceId $azureresc.ResourceId -DetailedOutput  -Status Succeeded | Where-Object OperationName -contains $azurerescop
+$azurerescvalues = Get-AzureRmLog -StartTime (Get-Date).AddDays(-89) -ResourceId $azureresc.ResourceId -DetailedOutput  -Status Succeeded | Where-Object OperationName -contains $azurerescop
 
 #sometimes the operations logs have multiple entries pick the latest record
 $owner = $azurerescvalues[0].Caller
+
+If ($owner -eq $null)
+{
+write-host "Owner equals null"
+} Else {
+
 
 #Obtain existing tags and apply existing Tags
 $tags = (Get-AzureRmResource -ResourceId $azureresc.ResourceId).Tags
 $tags += @{Owner=$owner}
 Set-AzureRmResource -ResourceId $azureresc.ResourceId -Tag $tags -Force
+
+}
 
 }

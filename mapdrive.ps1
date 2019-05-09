7e0c8bded609062fc43e7066238e6ca93268ef56
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2019-05-09 11:05:13 -0400

    Add files via upload

---
+++ mapdrive.ps1
@@ -0,0 +1,13 @@
+﻿param
+(
+    [Parameter (Mandatory=$false)]
+    [object] $fscredobj,
+
+    [string] $fsurl
+)
+
+Remove-PSDrive -Name S -Force
+
+start-sleep -Seconds 60
+
+New-PSDrive -Name S -PSProvider FileSystem -Root "\\$($fsurl)\samplefileshare" -Credential $fscredobj -Persist

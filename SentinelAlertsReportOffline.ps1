a923dbff54b1a4e58f631dab1dec8844b63a6788
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2020-02-11 14:32:07 -0500

    Update SentinelAlertsReportOffline.ps1

--- SentinelAlertsReportOffline.ps1
+++ SentinelAlertsReportOffline.ps1
@@ -10,7 +10,7 @@
 Invoke-WebRequest -Uri "https://github.com/Azure/Azure-Sentinel/archive/master.zip" -OutFile C:\temp\sentinel.zip
 
 # Extract the Sentinel Github .zip
-Expand-Archive -Path C:\temp\sentinel.zip -DestinationPath c:\temp\sentinel
+Expand-Archive -Path C:\temp\sentinel.zip -DestinationPath c:\temp\sentinel -Force
 
 # New Report
 $filename = "SentinelAlertsReport.csv"
@@ -21,28 +21,43 @@ $basepath = "C:\temp\sentinel\Azure-Sentinel-master\Detections"
 # Get all the detection .ayml rules
 $files = Get-ChildItem -Path C:\temp\sentinel\Azure-Sentinel-master\Detections -Recurse -File -Include "*.yaml"
 
+# for testing
+#$files = Get-ChildItem -Path C:\temp\sentinel\Azure-Sentinel-master\Detections
+
 # For each yaml rule get unique field from the rule and import them into the csv file.
 foreach ($file in $files){
+
+    # obtain the unique file's path and https:// link
     $pathfile = $basepath + "\" + $file.Directory.Name + "\" + $file.Name
     $linkpath = "https://github.com/Azure/Azure-Sentinel/tree/master/Detections/" + $file.Directory.Name + "/" + $file.Name
 
+    # obtain detection rule yaml and convert from yaml for extracting certain columns and reporting
     $rule = [pscustomobject](Get-Content $pathfile -Raw | ConvertFrom-Yaml)
 
+    # Create a new null object to collect detection information into
     $reportObj = New-Object PSCustomObject
 
-    $reportObj | add-member -NotePropertyName Name -NotePropertyValue $rule.Name
-    $reportObj | add-member -NotePropertyName Severity -NotePropertyValue $rule.Severity
+    # string value
+    $reportObj | Add-Member -NotePropertyName Name -NotePropertyValue $rule.Name
+    $reportObj | Add-Member -NotePropertyName Severity -NotePropertyValue $rule.Severity
 
-    ## Solve for multiple objects
-    $reportObj | add-member -NotePropertyName Tactics -NotePropertyValue $rule.tactics
-    $reportObj | add-member -NotePropertyName Connectors -NotePropertyValue $rule.requiredDataConnectors.connectorId
-    $reportObj | add-member -NotePropertyName Logs -NotePropertyValue $rule.requiredDataConnectors.dataTypes
-    $reportObj | add-member -NotePropertyName Techniques -NotePropertyValue $rule.relevantTechniques    
-    
-    $reportObj | add-member -NotePropertyName Description -NotePropertyValue $rule.description
-    $reportObj | add-member -NotePropertyName Link -NotePropertyValue $linkpath
+    ##  string array values
+    $reportObj | Add-Member -Type NoteProperty -Name Tactics -value ($rule.tactics -join '/')
+    $reportObj | Add-Member -Type NoteProperty -Name Connectors -value ($rule.requiredDataConnectors.connectorId -join '/')
+    $reportObj | Add-Member -Type NoteProperty -Name Logs -value ($rule.requiredDataConnectors.dataTypes -join '/') 
+    $reportObj | Add-Member -Type NoteProperty -Name Techniques -value ($rule.relevantTechniques -join '/')
 
+    # string value
+    $reportObj | Add-Member -NotePropertyName Description -NotePropertyValue $rule.description
+    $reportObj | Add-Member -NotePropertyName Link -NotePropertyValue $linkpath
 
+    # obtain last modified date on detection
+    $webRequest = [System.Net.HttpWebRequest]::Create($linkpath)
+    $webResponse = $webRequest.GetResponse()
+    $datetimestamp = ($webResponse.LastModified).ToShortDateString()
+    $reportObj | Add-Member -NotePropertyName LastModified -NotePropertyValue $datetimestamp
+
+    # write out unique detection and collected properties into .csv report
     $reportObj | Export-Csv C:\temp\$filename -NoTypeInformation -Delimiter "," -append
 
 }
